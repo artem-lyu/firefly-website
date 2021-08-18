b6868253c51c90b756c226b2b499458c647d1508
artem-lyu <lyuartem@gmail.com>
2021-08-18 22:18:51 +0800

    messed with b67 revision file to remove location

---
+++ migrations/versions/56b236a68648_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 56b236a68648
+Revises: 
+Create Date: 2021-08-18 22:12:50.362534
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '56b236a68648'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
