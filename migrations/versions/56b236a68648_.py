c07ba6893ea3dd8b2186f413659139ded9edff1c
artem-lyu <lyuartem@gmail.com>
2021-08-18 23:45:57 +0800

    finally 3??

---
+++ migrations/versions/56b236a68648_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 56b236a68648
+Revises: 
+Create Date: 2021-08-18 22:12:50.362534
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '56b236a68648'
+down_revision = '15c9715cfd02'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
